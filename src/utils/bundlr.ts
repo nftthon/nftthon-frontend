c8da9a9f836a3dbe6fe082170d8927d1c5aa4d0b
masa <50643303+mst-projects@users.noreply.github.com>
2023-03-11 13:07:05 +0700

    delete unused file

--- src/utils/bundlr.ts
+++
@@ -1,7 +0,0 @@
-import { WebBundlr } from '@bundlr-network/client';
-import { useAnchorWallet } from '@solana/wallet-adapter-react';
-
-const wallet = useAnchorWallet();
-export const bundlr = new WebBundlr("http://node1.bundlr.network", "solana", wallet);
-
-console.log(bundlr.address)
